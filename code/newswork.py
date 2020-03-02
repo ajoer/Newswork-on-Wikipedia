9a5e72049a7d9875056d56d26b8811268e3ea1dc
Anna Katrine Jørgensen <jorgensen@wcw-self-145-109-127-7.wireless.uva.nl>
2020-03-02 13:55:07 +0100

    fixing visualizations

--- code/newswork.py
+++ code/newswork.py
@@ -13,61 +13,114 @@ import re
 import revision_analysis
 import string
 import scipy
+
 import utils
 import utils_io as uio
 import utils_visualization as uviz
 
-from collections import defaultdict, Counter
+from collections import Counter, defaultdict, OrderedDict
+from datetime import datetime
 from Levenshtein import distance as levenshtein_distance
 from sklearn import preprocessing
 
 parser = argparse.ArgumentParser(description='''Quantitatively analyze the edit histories of Wikipedia language versions for newsiness and encyclopedia-ness.''')
 parser.add_argument("topic", help="e.g. 'refugee_crisis'.")
+parser.add_argument("--users", default="n")
+parser.add_argument("--temporal", default="n")
 parser.add_argument("--language", help="e.g. 'nl' (for debugging).")
 parser.add_argument("--visualize", default="n", help="e.g. 'y' (for debugging).")
 
 args = parser.parse_args()
 
-def perform_analyses():
+elements = ["links", "urls", "content"] # "images", "categories"]
+languages = [l for l in uio.get_language(args.topic)] #["sv", "da", "de", "sv", "es", "fr", "nb", "nl", "en"] # 
+#half = round(len(languages))
+#languages = languages[:half]
+comparative_languages = ["en", "it", "zh"]
+all_languages_totals = OrderedDict()
+all_languages_dates = dict()
 
-	for language in uio.get_language(args.topic):
+def perform_analyses(visualize=False):
+
+	for language in languages:
 		if args.language:
 			if language != args.language: continue
 
 		input_data = uio.open_file(args.topic, language)
+		if input_data == None: 
+			languages.remove(language)
+			continue
 		directory_name = "visualizations/%s/" % args.topic
 		uio.mkdirectory(directory_name)
 
-		ra = revision_analysis.Analyze(input_data, language, args.topic)
-		
+		ra = revision_analysis.Analyze(input_data, language, args.topic, daily=True)
+		if language not in all_languages_dates.keys():
+			language_timestamps = ra.timestamps
+			all_languages_dates[language] = language_timestamps
+
 		# - - - - Perform analyses per element - - - - 
-		elements = ["links", "urls", "images", "content", "categories"] # "sections" have a bad format for now: [[],[],[]]
-		totals = []
+
+		# todo: "sections" have a bad format for now: [[],[],[]]
+		totals_temporal = []
 		analysis_data = defaultdict()
+
+		if args.visualize == "y":
+			visualize = True
 
 		for element in elements:
 			if element == "content": 
-				added, removed, total = ra.string_development(element, visualize=True)
+				added, removed, totals = ra.string_development(element, visualize=visualize)
 			else: 
-				added, removed, total = ra.list_development(element, visualize=True)
-
+				added, removed, totals = ra.list_development(element, remove_vandalism=True, visualize=visualize)
 			analysis_data[element] = [added,removed]
-			totals.append(total)
+			totals_temporal.append(totals)
 
-		# Sections
+		all_languages_totals[language] = totals_temporal
+
+		# Users
+		if args.users == "y":
+			users = ra.get_users()
+			total_edits = sum(users.values())
+			total_users = len(users)
+			median_users = len([k for (k,v) in users.items() if v ==1])
+			print("users\t\t", total_users)
+			print("max contributions", users.most_common(1)[0][1])
+			print("average:\t", round(total_edits/total_users,2))
+			print("median:\t\t", sorted(users.values())[round(len(users)/2)], "value, and %s percent" %round(median_users/total_users, 2))
 
 		# - - - - Temporal overview all elements - - - - -  
-		# Added/Removed over time for all elements
-		uviz.plot_article_development(ra.dates, analysis_data.values(), elements, args.topic, language, "article_changes")
-		#uviz.plot_article_development(analysis_object.dates, totals, elements, args.topic, language, "total_development")
 
-		# General development of article over time, needs scaling to accomodate the differences in scope
-		mm_scaler = preprocessing.MinMaxScaler()
-		scaled_totals = [mm_scaler.fit_transform(np.array(x).reshape(-1, 1)) for x in totals]
-		try:
-			uviz.plot_article_development(ra.dates, scaled_totals, elements, args.topic, language, "article_development")
-		except ValueError as err:
-			print("there is an error hier", err)
+		if args.temporal == "y":
+			# Added/Removed over time for all elements
+			y = [datetime.strptime(ts, "%Y-%m-%dT%H:%M:%SZ") for ts in language_timestamps]
+			uviz.plot_article_development(y, analysis_data.values(), elements, args.topic, language, "article_changes")
 
+			# General development of article over time, needs scaling to accomodate the differences in scope
+			mm_scaler = preprocessing.MinMaxScaler()
+			scaled_totals = [mm_scaler.fit_transform(np.array(x).reshape(-1, 1)) for x in totals_temporal]
+			try:
+				uviz.plot_article_development(y, scaled_totals, elements, args.topic, language, "article_development")
+			except ValueError as err:
+				print("Couldn't plot article development because:\t", err)
+	
+def comparative():
+	##### Make timestamps datetime expressions in wikirecparser, so math comp is possible, e.g. <>=
+	# that will make it possible to plot more languages in one and change labels to Jan 2010.
+	for n, element in enumerate(elements):
+		print("n",n)
+		plotting_data = []
+		for language in comparative_languages: 
+			print("language", language)
+			if args.language:
+				if language != args.language: continue
+				language = args.language
+
+			lang_data = all_languages_totals[language][n]
+			plotting_data.append(lang_data)
+
+		uviz.plot_element_across_languages(all_languages_dates, plotting_data, element, languages, args.topic, "comp_%s" % element)
+
+		
 if __name__ == "__main__":
 	perform_analyses()
+	comparative()
--- code/utils_visualization.py
+++ code/utils_visualization.py
@@ -3,14 +3,16 @@
 	Visualization utils for newswork analysis module.
 """
 import matplotlib.pyplot as plt
+import matplotlib._color_data as mcd
 import numpy as np
 import utils_io as uio
 
-from scipy.interpolate import make_interp_spline, BSpline
+from datetime import datetime
+from sklearn.preprocessing import Normalizer
 
-colors = ["indianred", "goldenrod", "steelblue", "darkolivegreen", "purple"]
+#colors = ["indianred", "goldenrod", "steelblue", "darkolivegreen", "purple", "red", "blue", "green", "orange", "yellow","indianred", "goldenrod", "steelblue", "darkolivegreen", "purple", "red", "blue", "green", "orange", "yellow","indianred", "goldenrod", "steelblue", "darkolivegreen", "purple", "red", "blue", "green", "orange", "yellow","indianred", "goldenrod", "steelblue", "darkolivegreen", "purple", "red", "blue", "green", "orange", "yellow"]
+colors = [mcd.XKCD_COLORS[c] for c in mcd.XKCD_COLORS]
 linestyles = ["solid", "dotted"]
-
 
 def get_RAWgraph_area_data(all_keys, input_dict):
 	print("date\tkey\tvalue")
@@ -27,18 +29,22 @@ def get_RAWgraph_barchart_data(input_dict, change_type):
 
 	for date in input_dict:
 		print(date, "\t", change_type, "\t", input_dict[date])
-	
 
 def plot_barchart(dates, added, removed, topic, language, analysis):
 	''' Plot changes to links. ''' 
-
 	plt.axhline(0, color='grey')
 	plt.xlabel('dates')
 	plt.ylabel('changes to %s' % topic)
+	#plt.xlim([min(dates), max(dates)])
+
+	# scale values for comparative visual analysis across languages
+	## NB: scaling here doesn't work for some reason
+	#added_transformed = Normalizer().fit_transform(np.array(added).reshape(1, -1))
+	#removed_transformed = Normalizer().fit_transform(np.array(removed).reshape(1, -1))
 	plt.bar(dates, added)
 	plt.bar(dates, removed)
-
-	labels = [dates[0]]+[x for n,x in enumerate(dates) if float(n) in [round(x) for x in np.arange(0+(len(dates)-2)/10, len(dates)-1, 5)]]+[dates[-1]]
+	
+	labels = [dates[0], dates[round(len(dates)/2)], dates[-1]] #dates[0]]+[x for n,x in enumerate(dates) if float(n) in [round(x) for x in np.arange(0+(len(dates)-2)/10, len(dates)-1, 5)]]+[dates[-1]]
 	plt.xticks(labels, rotation = 45)
 	plt.tight_layout()
 
@@ -48,25 +54,33 @@ def plot_barchart(dates, added, removed, topic, language, analysis):
 	plt.savefig(directory_name + file_name)
 
 	plt.close()
-
+ 
 def plot_article_development(dates, elements, titles, topic, language, analysis):
 	
 	fig, ax = plt.subplots()
-	plt.xlabel('time')
+	plt.xlabel('Time')
 
 	for n, element in enumerate(elements):
 		if len(element) == 2: # For addition/removal development visualization: [ [1, 0, 0, 2, 0], [0, 0, -3, 0, 0] ]
-			plt.ylabel('values')
-			ax.plot(dates, element[0], label="+ %s" % titles[n], linestyle=linestyles[0], color=colors[n])
-			ax.plot(dates, element[1], label="- %s" % titles[n], linestyle=linestyles[1], color=colors[n])
+			plt.ylabel('Values')
+
+			# scale for comparitive visual analysis between languages
+			X0_transformed = Normalizer().fit_transform(np.array(element[0]).reshape(1, -1))
+			X1_transformed = Normalizer().fit_transform(np.array(element[1]).reshape(1, -1))
+	
+			ax.plot(dates, X0_transformed.transpose(), label="+ %s" % titles[n], linestyle=linestyles[0], color=colors[n])
+			ax.plot(dates, X1_transformed.transpose(), label="- %s" % titles[n], linestyle=linestyles[1], color=colors[n])
 		else: # For general article development visualization: [1, 0, 0, 2, 0]
-			plt.ylabel('scaled values')
-			ax.plot(dates, element, label=titles[n], linestyle=linestyles[0], color=colors[n])
+			plt.ylabel('Normalized values')
+			# normalize values to same space:
+			# NB: also doesn't work
+			X_transformed = Normalizer().fit_transform(np.array(element).reshape(1, -1))
+			ax.plot(dates, X_transformed.transpose(), label=titles[n], linestyle=linestyles[0], color=colors[n])
 	
 		ax.legend(loc='center left', bbox_to_anchor=(1, 0.5))
 
-	labels = [dates[0]]+[x for n,x in enumerate(dates) if float(n) in [round(x) for x in np.arange(0+(len(dates)-2)/10, len(dates)-1, 10)]]+[dates[-1]]
-	plt.xticks(labels, rotation = 45)
+	labels = [dates[0], dates[len(dates)/4], dates[len(dates)/2], dates[(len(dates)/4)*3], dates[-1] #[dates[0]]+[x for n,x in enumerate(dates) if float(n) in [round(x) for x in np.arange(0+(len(dates)-2)/10, len(dates)-1, 10)]]+[dates[-1]]
+	plt.xticks(labels, rotation = 90)
 	plt.tight_layout()
 
 	directory_name = "visualizations/%s/%s/" % (topic, analysis)
@@ -75,6 +89,36 @@ def plot_article_development(dates, elements, titles, topic, language, analysis)
 	plt.savefig(directory_name + file_name)
 
 	plt.close()
+
+def plot_element_across_languages(dates, data, element, languages, topic, language):
+
+	fig, ax = plt.subplots()
+	plt.xlabel('time')
+	plt.title("%s" % element)
+
+	label_dates = []
+	for n, lang_data in enumerate(data):
+
+		language = languages[n]
+		# get dates for labels:
+		lang_dates = [datetime.strptime(ts, "%Y-%m-%dT%H:%M:%SZ") for ts in dates[languages[n]]]
+		label_dates += lang_dates
+
+		# normalize values to same space:
+		X_transformed = Normalizer().fit_transform(np.array(lang_data).reshape(1, -1)) #, np.array(lang_dates))
+		plt.ylabel('values')
+		ax.plot(lang_dates, X_transformed.transpose(), label=language, color=colors[n])
+		ax.legend(loc='center left', bbox_to_anchor=(1, 0.5))
+
+	#plt.xticks(labels, rotation = 45)
+	plt.tight_layout()
+	plt.show()
+	# directory_name = "visualizations/%s/%s/" % (topic, analysis)
+	# uio.mkdirectory(directory_name)
+	# file_name = "%s.png" % language
+	# plt.savefig(directory_name + file_name)
+
+	# plt.close()
 
 if __name__ == "__main__":
 	plot_barchart()
